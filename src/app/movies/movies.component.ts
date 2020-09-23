e85348dbc235a8387720ab6e336d73c2213d0095
ayben.manici <ayben.manici@IT-AYBENM>
2020-09-23 23:07:01 +0300

    Lesson1: Generate Component

---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+
+    selector:'movies', // componentı cagıracagız
+    template:'movies.component.html'
+})
+export class MoviesComponent{
+
+
+
+
+
+}
